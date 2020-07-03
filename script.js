e62dd6ed7a4afdffb85508bfa5e9709540208e1d
litmiz <litmiz07@gmail.com>
2020-07-03 22:07:17 +0300

    Add load photos

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ response.json().then(data => {
     data.forEach(element => {
         document.getElementById('albumsArea').innerHTML += `
         <button class="albumBtn" onclick="loadPic(${element.id})">${element.title} album</button>
+        <div id="picAlbum${element.id}Area"></div>
         ` 
     }); 
 }));
+///////////////////////////////////////////////////////////
+
+
+// load photos
+function loadPic(albumId) {
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`).then(response =>
+    response.json().then(data => {
+    data.forEach(element => {
+        document.getElementById(`picAlbum${albumId}Area`).innerHTML += `
+        <h3>${element.title}</h3>
+        <img src="${element.url}">
+        `
+    });
+}));
+}
 ///////////////////////////////////////////////////////////
