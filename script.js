a44ec23a53e1d01c904cef9e2f8c593d348c26a7
Asaf Amnony <aamnony@gmail.com>
2020-07-03 22:49:48 +0300

    Added album creator name

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Load all albums
 fetch('https://jsonplaceholder.typicode.com/albums').then(response =>
-response.json().then(data => {
-    data.forEach(element => {
-        document.getElementById('albumsArea').innerHTML += `
-        <button class="albumBtn" onclick="loadPic(${element.id})">${element.title} album</button>
-        <div id="picAlbum${element.id}Area"></div>
-        ` 
-    }); 
+response.json().then(albumData => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response2 =>
+        response2.json().then(usersData => {
+            albumData.forEach(element => {
+                let userNames = usersData.filter(function (user) {
+                    return user.id == element.userId;
+                });
+
+                document.getElementById('albumsArea').innerHTML += `
+                <button class="albumBtn" onclick="loadPic(${element.id})">${element.title} album
+                <p>This album was created by ${userNames[0].name}</p></button>
+                <span id="picAlbum${element.id}Area"></span>
+                ` 
+            }); 
+        }));
 }));
 ///////////////////////////////////////////////////////////
 
@@ -17,8 +25,10 @@ function loadPic(albumId) {
     response.json().then(data => {
     data.forEach(element => {
         document.getElementById(`picAlbum${albumId}Area`).innerHTML += `
-        <h3>${element.title}</h3>
-        <img src="${element.url}">
+        <div class="picDiv">
+            <h3>${element.title}</h3>
+            <img src="${element.url}">
+        </div>
         `
     });
 }));
